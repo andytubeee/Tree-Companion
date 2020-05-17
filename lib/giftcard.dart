327a697f1cc9462f4e8be91fc5314dc747bf8d49
Andrew Yang <andy.tubeee@gmail.com>
2020-05-17 00:55:41 -0400

    added spending giftcard

---
+++ lib/giftcard.dart
@@ -0,0 +1,45 @@
+import 'package:Tree_Companion/wallet.dart';
+import 'package:flutter/material.dart';
+
+class GiftCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GiftCardApp(),
+    );
+  }
+}
+
+class GiftCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            new Image.asset("assets/giftcardPG.png", width: MediaQuery.of(context).size.width,),
+            new Container(
+            margin: EdgeInsets.only(top: 660, left: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Wallet()),
+                    );
+                  },
+                  child: Icon(Icons.arrow_left),
+                  backgroundColor: Colors.green,
+                )
+              ],
+            ),
+          ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/wallet.dart
+++ lib/wallet.dart
@@ -1,3 +1,4 @@
+import 'package:Tree_Companion/giftcard.dart';
 import 'package:flutter/material.dart';
 import 'leaderboard.dart';
 
@@ -14,24 +15,33 @@ class WalletApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.greenAccent,
         body: Container(
       child: Stack(
         children: <Widget>[
-          new Image.asset("assets/WalletPG.png", width: MediaQuery.of(context).size.width,),
-          new Container(
-            //Spend Button
-            margin: EdgeInsets.only(top: 220, left: 20),
-            width: 150,
-            height: 100,
-            child: FlatButton(onPressed: (){}, child: Text(""))
+          new Image.asset(
+            "assets/WalletPG.png",
+            width: MediaQuery.of(context).size.width,
           ),
           new Container(
-            //Transfer Button
-            margin: EdgeInsets.only(top: 220, left: 190),
-            width: 150,
-            height: 100,
-            child: FlatButton(onPressed: (){}, child: Text(""))
-          ),
+              //Spend Button
+              margin: EdgeInsets.only(top: 220, left: 20),
+              width: 150,
+              height: 100,
+              child: FlatButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GiftCard()),
+                    );
+                  },
+                  child: Text(""))),
+          new Container(
+              //Transfer Button
+              margin: EdgeInsets.only(top: 220, left: 190),
+              width: 150,
+              height: 100,
+              child: FlatButton(onPressed: () {}, child: Text(""))),
           new Container(
             margin: EdgeInsets.only(top: 660, left: 20),
             child: Row(
@@ -40,9 +50,9 @@ class WalletApp extends StatelessWidget {
                 new FloatingActionButton.extended(
                   onPressed: () {
                     Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Leaderboard()),
-                );
+                      context,
+                      MaterialPageRoute(builder: (context) => Leaderboard()),
+                    );
                   },
                   label: Text("Back"),
                   icon: Icon(Icons.arrow_left),
